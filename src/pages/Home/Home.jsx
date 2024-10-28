7e0f6f7fc2fb07de0ccbded8153c19ea41103a33
eungyo <eungyo0965@gmail.com>
2024-10-28 13:13:21 +0100

    feat (home) : update homepage with css styling

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,21 @@
 import './Home.css'
 import Navbar from '../../components/Navbar/Navbar'
+import hero_banner from '../../assets/hero_banner.jpg'
+import hero_title from '../../assets/hero_title.png'
 
 const Home = () => {
   return (
     <div className="home">
         <Navbar />
+        <div className='hero'>
+            <img src={hero_banner} alt="" className='banner-img' />
+            <div className='hero-caption'>
+                <img src={hero_title} alt="" className='caption-img' />
+                <p>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus voluptatibus quidem distinctio delectus illum? Quibusdam id unde accusantium provident recusandae pariatur ab, soluta sequi non. Eum, exercitationem! Cupiditate, tempora amet.
+                </p>
+            </div>
+        </div>
     </div>
   )
 }
