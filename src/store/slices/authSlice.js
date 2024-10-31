3e869fc173c67c59878b768e5a05b89812b95079
eungyo <eungyo0965@gmail.com>
2024-10-31 14:47:12 +0100

    feat(authSlice) : Define state, actoins

---
+++ src/store/slices/authSlice.js
@@ -0,0 +1,38 @@
+//상태와 리듀서, 액션을 하나의 Slice로 묶어 관리
+//상태와 액션을 정의
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+    email: '',
+    password: '',
+    isLoggedIn: false,
+    error: '',
+};
+
+const authSlice = createSlice({
+    name: 'auth',
+    initialState,
+    reducers: {
+        setEmail: (state, action) => {
+        state.email = action.payload;
+        },
+        setPassword: (state, action) => {
+        state.password = action.payload;
+        },
+        setError: (state, action) => {
+        state.error = action.payload;
+        },
+        loginSuccess: (state) => {
+        state.isLoggedIn = true;
+        state.error = '';
+        },
+        logout: (state) => {
+        state.isLoggedIn = false;
+        state.email = '';
+        state.password = '';
+        },
+    },
+});
+
+export const { setEmail, setPassword, setError, loginSuccess, logout } = authSlice.actions;
+export default authSlice.reducer;
