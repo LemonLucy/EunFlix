1386416a2246b88992eeada82b7317592bd310a8
eungyo <eungyo0965@gmail.com>
2024-10-31 14:52:23 +0100

    feat(app) : Send store to Provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import Home from "./pages/Home/Home"
 import {Routes,Route } from "react-router-dom"
 import LoginWrapper from "./pages/Login/LoginWrapper"
+import store from "./store";
+import { Provider } from 'react-redux';
 
 const App = () => {
   return (
-    <div>
+    <Provider store={store}>
+      <div>
         <Routes>
           <Route path='/' element={<Home/>} />
           <Route path='/login' element={<LoginWrapper/>} />
         </Routes>
-    </div>
+      </div>
+    </Provider>
+    
   )
 }
 
