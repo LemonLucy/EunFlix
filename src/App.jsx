30072e41993cf5155e721ccbf554ab3871ae2e4d
eungyo <eungyo0965@gmail.com>
2024-11-01 14:42:24 +0100

    feat(login) : Keep Login status

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,29 @@ import {Routes,Route } from "react-router-dom"
 import LoginWrapper from "./pages/Login/LoginWrapper"
 import store from "./store";
 import { Provider } from 'react-redux';
+import PrivateRoute from "./components/Auth/PrivateRoute";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { loginSuccess } from "./store/actions/authActions";
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const storedUser = JSON.parse(localStorage.getItem('user-info'));
+    const isAuthenticated = JSON.parse(localStorage.getItem('isAuthenticated'));
+
+    if (storedUser && isAuthenticated) {
+      dispatch(loginSuccess(storedUser));
+    }
+  }, [dispatch]);
+
   return (
     <Provider store={store}>
-          <Routes>
-            <Route path='/' element={<Home/>} />
-            <Route path='/login' element={<LoginWrapper/>} />
-          </Routes>
+      <Routes>
+        <Route path='/' element={<PrivateRoute><Home /></PrivateRoute>} />
+        <Route path='/login' element={<LoginWrapper/>} />
+      </Routes>
     </Provider>
     
   )
---
+++ src/components/Auth/PrivateRoute.jsx
@@ -0,0 +1,17 @@
+import { Navigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import PropTypes from 'prop-types';
+
+const PrivateRoute = ({ children }) => {
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+
+  // 로그인 상태가 아니면 Login 페이지로 리디렉션
+  return isAuthenticated ? children : <Navigate to="/login" />;
+};
+
+
+PrivateRoute.propTypes = {
+    children: PropTypes.node.isRequired,  // children prop에 대한 유효성 검사 추가
+  };
+
+export default PrivateRoute;
