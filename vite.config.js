2576de430d387ebb82b0e3c1d4e113a981f04719
eungyo <eungyo0965@gmail.com>
2024-12-26 11:36:33 +0100

    edit vite config

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,13 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-  // mode에 따라 직접 파일 지정
-  const envFile = mode === 'production' ? '.env.prod' : '.env.dev';
+  // mode에 따라 환경 변수 로드
   const env = loadEnv(mode, process.cwd(), '');
 
   return {
     plugins: [react()],
     define: {
-      'import.meta.env': JSON.stringify(env), // 환경 변수를 import.meta.env에 매핑
+      'import.meta.env': env, // 환경 변수를 import.meta.env에 매핑
     },
   };
 });
