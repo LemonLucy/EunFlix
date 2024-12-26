b2139cdffeb8b408b253552c681ffd17c2e972f3
eungyo <eungyo0965@gmail.com>
2024-12-26 12:19:33 +0100

    edit vite config

--- src/hooks/useLogin.jsx
+++ src/hooks/useLogin.jsx
@@ -49,9 +49,18 @@ const loginWithKakao = async (user) => {
       return;
     }
 
+    // 사용자 정보 객체에 profileImage 추가
+    const updatedUser = {
+      ...user,
+      profileImage: user.profileImage || '', // profileImage가 없는 경우 기본값 설정
+    };
+
     // Redux 상태 업데이트
-    dispatch(loginSuccess(user));
+    dispatch(loginSuccess(updatedUser));
     await showToast('Success', 'Kakao Login successful!', 'success');
+
+    // LocalStorage에 사용자 정보 저장
+    localStorage.setItem('userInfo', JSON.stringify(updatedUser));
 
     // 홈으로 리디렉션
     navigate('/');
@@ -59,6 +68,7 @@ const loginWithKakao = async (user) => {
     console.error('Kakao Login error:', error);
   }
 };
+
 
 return { login, loginWithKakao };
 };
